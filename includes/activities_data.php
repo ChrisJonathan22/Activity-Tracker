04220a1fd317cb1ac0e2e4258328516c43555e76
Chris Jonathan <christophereko@hotmail.fr>
2020-02-12 19:58:26 +0000

    I've retrieved the data from the database and I've displayed it on the page.

--- includes/activities_data.php
+++ includes/activities_data.php
@@ -1,10 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php 
 
+include_once './db.inc.php';
+
+
 $conn = new Database();
-$conn = $conn->connect();
 
 $query = "SELECT * FROM activity;";
 
-msqli_query($conn->connect())
+$result = mysqli_query($conn->connect(), $query);
+$resultCheck = mysqli_num_rows($result);
+
+if ($resultCheck > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "<p>Activity name: " . $row["activity_name"]. " - Hours: " . $row["activity_hours"]. " Minutes:" . $row["activity_minutes"]. "<br></p>";
+    }
+} else {
+    echo "0 results";
+}
+
 
 ?>
+</body>
+</html>
