6d8ed14107e7fd9b908812ad863d57ad5f72fdc2
Chris Jonathan <christophereko@hotmail.fr>
2020-02-13 23:10:35 +0000

    I'm working on adding each activity's data and then display it within the chart.

--- includes/activities_data.php
+++ includes/activities_data.php
@@ -18,13 +18,52 @@ $query = "SELECT * FROM activity;";
 $result = mysqli_query($conn->connect(), $query);
 $resultCheck = mysqli_num_rows($result);
 
+$fitness = 0;
+$leisure = 0;
+$work = 0;
+$eating = 0;
+$reading = 0;
+$sleeping = 0;
+
+
 if ($resultCheck > 0) {
     while($row = $result->fetch_assoc()) {
         echo "<p>Activity name: " . $row["activity_name"]. " - Hours: " . $row["activity_hours"]. " Minutes:" . $row["activity_minutes"]. "<br></p>";
+
+        switch ($row["activity_name"]) {
+            case "Fitness":
+                $fitness += $row["activity_hours"];
+            break;
+
+            case "Leisure":
+                $leisure += $row["activity_hours"];
+            break;
+
+            case "Work":
+                $work += $row["activity_hours"];
+            break;
+
+            case "Eating":
+                $eating += $row["activity_hours"];
+            break;
+
+            case "Reading":
+                $reading += $row["activity_hours"];
+            break;
+
+            case "Sleeping":
+                $sleeping += $row["activity_hours"];
+            break;
+
+            default:
+                echo "Unknown activity";
+        }
     }
 } else {
     echo "0 results";
 }
+
+echo "Fitness hours " . $fitness;
 
 
 ?>
